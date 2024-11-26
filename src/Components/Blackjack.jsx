28524e7124d3ecea714efe56de2bf68b2654c9ec
Jay Dillon <jsdillon04@gmail.com>
2024-11-26 14:24:25 -0800

    card API/Blackjack started

--- src/Components/Blackjack.jsx
+++ src/Components/Blackjack.jsx
@@ -1,10 +1,54 @@
 import GameHeader from "./GameHeader"
+import { useEffect, useState } from "react"
+import { getDeck, getCard } from "../Services/cardService"
 
 export default function Blackjack() {
+
+    const [betting, setBetting] = useState(true)
+    const [bet, setBet] = useState(0)
+    const [currentDeck, setCurrentDeck] = useState("nope")
+    const [playerCards, setPlayerCards] = useState([])
+    const [dealerCards, setDealerCards] = useState([])
+    const [drawing, setDrawing] = useState(false)
+
+    function placeBet(e){
+        e.preventDefault()
+        //link to database
+        setBetting(false)
+        setDrawing(true)
+    }
+
+    async function startingDraw(){
+        //const deck = await getDeck()
+        //setCurrentDeck(await getDeck())
+        getDeck().then(setCurrentDeck)
+        console.log(currentDeck)
+        //setPlayerCards([await getCard(currentDeck), await getCard(currentDeck)])
+        //setDealerCards([await getCard(currentDeck), await getCard(currentDeck)])
+        //console.log(playerCards)
+        //console.log(dealerCards)
+        setDrawing(false)
+
+    }
+
+    function scoreConvert(){
+
+    }
 
     return (
         <>
             <GameHeader title ="Blackjack" />
+            {betting ?(
+                <form onSubmit={placeBet}>
+                <h1>Place your bet: </h1>
+                <input type ="number" onChange={(e) => setBet(e.target.value)}/>
+                </form>
+            ): drawing ?(<> 
+
+                <button onClick={() => startingDraw()}>test</button>
+
+            
+            </>) : (<></>)}
         </>
     )
 }
