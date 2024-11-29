ac435739d0d66cf148fed25330798b3add3b088d
Jay Dillon <jsdillon04@gmail.com>
2024-11-29 14:37:05 -0800

    Roulette 80% completed, needs format fix and score calculation then make look good

--- src/Components/Roulette.jsx
+++ src/Components/Roulette.jsx
@@ -1,11 +1,82 @@
 import GameHeader from "./GameHeader"
+import RouletteBet from "./RouletteBet"
+import { useEffect, useState } from "react"
 
 export default function Roulette() {
+    
+    const possibilities = ['0', '00', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36']
+    const red = ['1', '3', '5', '7', '9', '12', '14', '16', '18', '19', '21', '23', '25', '27', '30', '32', '34', '36']
+    
+    const [spinValue, setSpinValue] = useState("37")
+    const [winners, setWinners] = useState([])
+    const [betList, setBetList] = useState(new Map())
+
+    function Spin (){
+        const randomIndex = Math.floor(Math.random() * possibilities.length)
+        setSpinValue(possibilities[randomIndex])
+      };
+    
+    function getWinners(){
+        var tempWinners = []
+        var spinNum = Number(spinValue)
+        
+        tempWinners.push(spinValue)
+
+        spinNum < 13 ? (
+            tempWinners.push("1st 12")
+        ) : spinNum < 25 ? (
+            tempWinners.push("2nd 12")
+        ) : (
+            tempWinners.push("3rd 12")
+        )
+
+        spinNum % 2 === 0 ?(
+            tempWinners.push("Even")
+        ) : (
+            tempWinners.push("Odd")
+        )
+
+        spinNum < 19 ? (
+            tempWinners.push("1 - 18")
+        ) : (
+            tempWinners.push("19 - 36")
+        )
+
+        red.includes(spinValue) ? (
+            tempWinners.push("Red")
+        ) : (
+            tempWinners.push("Black")
+        )
+
+        spinNum % 3 === 0 ? (
+            tempWinners.push("3rd col")
+        ) : spinNum % 3 === 1 ? (
+            tempWinners.push("1st col")
+        ) : (
+            tempWinners.push("2nd col")
+        )
+
+        setWinners(tempWinners)
+    }
+
+
+    useEffect(() => getWinners(), [spinValue])
 
     return (
         <>
             <GameHeader title ="Roulette" />
-        </>
+            
+            {spinValue < 37 ? (
+                <>{spinValue}       {winners}</>
+            ) : (
+                <></>
+            )}
+            <div class="Roulette">
+            <button onClick={() => Spin()}> spin</button>
+
+            <RouletteBet betList={betList} setBetList={setBetList}/>
+            </div>
+             </>
     )
 
 }
---
+++ src/Components/RouletteBet.jsx
@@ -0,0 +1,140 @@
+import { useEffect, useState } from "react"
+
+
+export default function RouletteBet({betList , setBetList}){
+
+    const [currentBet, setCurrrentBet] = useState("")
+    const [betValue, setBetValue] = useState(0)
+
+    const [tempBet, settempBet] = useState(0)
+
+    function updateBet(toAdd){
+        var tempBetList = betList
+        
+        betList.has(currentBet) ? (
+            tempBetList.set(currentBet, betList.get(currentBet) + toAdd)
+        ) : (
+            tempBetList.set(currentBet, toAdd)
+        )
+        setBetList(tempBetList)
+    }
+
+    function clearEntry(){
+        var tempBetList = betList
+
+        tempBetList.delete(currentBet)
+
+        setBetList(tempBetList)
+    }
+
+    function endbetting(){
+        {bet(tempBet)}
+        {flag()}
+    }
+
+    useEffect(() => {setBetValue(betList.get(currentBet))} ,[betList,currentBet])
+
+    return (<>
+        <div class="top">
+        <div class="zeros">
+        <button onClick={() => setCurrrentBet("0")}>0</button>
+        <button onClick={() => setCurrrentBet("00")}>00</button>
+        </div>
+
+        <div class="middle">
+        <button onClick={() => setCurrrentBet("1")}>1</button>
+        <button onClick={() => setCurrrentBet("2")}>2</button>
+        <button onClick={() => setCurrrentBet("3")}>3</button>
+        <button onClick={() => setCurrrentBet("4")}>4</button>
+        <button onClick={() => setCurrrentBet("5")}>5</button>
+        <button onClick={() => setCurrrentBet("6")}>6</button>
+        <button onClick={() => setCurrrentBet("7")}>7</button>
+        <button onClick={() => setCurrrentBet("8")}>8</button>
+        <button onClick={() => setCurrrentBet("9")}>9</button>
+        <button onClick={() => setCurrrentBet("10")}>10</button>
+        <button onClick={() => setCurrrentBet("11")}>11</button>
+        <button onClick={() => setCurrrentBet("12")}>12</button>
+        <button onClick={() => setCurrrentBet("13")}>13</button>
+        <button onClick={() => setCurrrentBet("14")}>14</button>
+        <button onClick={() => setCurrrentBet("15")}>15</button>
+        <button onClick={() => setCurrrentBet("16")}>16</button>
+        <button onClick={() => setCurrrentBet("17")}>17</button>
+        <button onClick={() => setCurrrentBet("18")}>18</button>
+        <button onClick={() => setCurrrentBet("19")}>19</button>
+        <button onClick={() => setCurrrentBet("20")}>20</button>
+        <button onClick={() => setCurrrentBet("21")}>21</button>
+        <button onClick={() => setCurrrentBet("22")}>22</button>
+        <button onClick={() => setCurrrentBet("23")}>23</button>
+        <button onClick={() => setCurrrentBet("24")}>24</button>
+        <button onClick={() => setCurrrentBet("25")}>25</button>
+        <button onClick={() => setCurrrentBet("26")}>26</button>
+        <button onClick={() => setCurrrentBet("27")}>27</button>
+        <button onClick={() => setCurrrentBet("28")}>28</button>
+        <button onClick={() => setCurrrentBet("29")}>29</button>
+        <button onClick={() => setCurrrentBet("30")}>30</button>
+        <button onClick={() => setCurrrentBet("31")}>31</button>
+        <button onClick={() => setCurrrentBet("32")}>32</button>
+        <button onClick={() => setCurrrentBet("33")}>33</button>
+        <button onClick={() => setCurrrentBet("34")}>34</button>
+        <button onClick={() => setCurrrentBet("35")}>35</button>
+        <button onClick={() => setCurrrentBet("36")}>36</button>
+        <button onClick={() => setCurrrentBet("1st col")}>2 - 1</button>
+        <button onClick={() => setCurrrentBet("2nd col")}>2 - 1</button>
+        <button onClick={() => setCurrrentBet("3rd col")}>2 - 1</button>
+        </div>
+
+        </div>
+
+        <div class="top_bottom">
+        <button onClick={() => setCurrrentBet("1st 12")}>1st 12</button>
+        <button onClick={() => setCurrrentBet("2nd 12")}>2nd 12</button>
+        <button onClick={() => setCurrrentBet("3rd 12")}>3rd 12</button>
+        </div>
+
+        <div class="bottom_bottom">
+        <button onClick={() => setCurrrentBet("1 - 18")}>1 - 18</button>
+        <button onClick={() => setCurrrentBet("Even")}>Even</button>
+        <button onClick={() => setCurrrentBet("Red")}>Red</button>
+        <button onClick={() => setCurrrentBet("Black")}>Black</button>
+        <button onClick={() => setCurrrentBet("Odd")}>Odd</button>
+        <button onClick={() => setCurrrentBet("19 - 36")}>19 - 36</button>
+        </div>
+
+
+        <footer>
+    
+    <div id="bet">
+    <img onClick={()=> updateBet(1)} src="https://brand.lmu.edu/media/wnmd/brand/identitystandards/marks-ceremonial/lmu-ceremonial-mark.png" width={100} height={100} />
+    
+    <img onClick={()=> updateBet(5)} src="https://brand.lmu.edu/media/wnmd/brand/identitystandards/marks-ceremonial/lmu-ceremonial-mark.png" width={100} height={100} />
+    
+    <img onClick={()=> updateBet(25)} src="https://brand.lmu.edu/media/wnmd/brand/identitystandards/marks-ceremonial/lmu-ceremonial-mark.png" width={100} height={100} />
+    
+    <img onClick={()=> updateBet(100)} src="https://brand.lmu.edu/media/wnmd/brand/identitystandards/marks-ceremonial/lmu-ceremonial-mark.png" width={100} height={100} />
+    
+    <img onClick={()=> updateBet(500)} src="https://brand.lmu.edu/media/wnmd/brand/identitystandards/marks-ceremonial/lmu-ceremonial-mark.png" width={100} height={100} />
+    
+    <img onClick={()=> updateBet(1000)} src="https://brand.lmu.edu/media/wnmd/brand/identitystandards/marks-ceremonial/lmu-ceremonial-mark.png" width={100} height={100} />
+    
+    <p> $1 </p>
+    <p> $5 </p>
+    <p> $25 </p>
+    <p> $100 </p>
+    <p> $500 </p>
+    <p> $1000 </p>
+    </ div>
+
+    <div id="betOutput">
+    <p>Bet on {currentBet}: ${betValue}</p>
+    <button onClick={()=> clearEntry()}>Clear</button>
+    <button onClick={()=> setBetList(new Map())}>Clear All</button>
+    </div>
+
+
+
+
+    </footer>
+
+    </>
+    )
+}
